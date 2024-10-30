d4f2190514172cb790db999876f93c6e25707f32
jalil <jalil@1001.digital>
2024-10-31 00:04:10 +0100

    Remove unneeded code

--- tasks/accounts.ts
+++ tasks/accounts.ts
@@ -1,6 +1,5 @@
 import { parseEther } from 'viem'
 import { task } from 'hardhat/config'
-import { JALIL } from '../helpers/constants'
 
 task('accounts', 'Prints the list of accounts', async (_, hre) => {
   const clients = await hre.viem.getWalletClients()
@@ -9,12 +8,4 @@ task('accounts', 'Prints the list of accounts', async (_, hre) => {
     console.log(client.account.address)
   }
 })
-
-task('fund-account', 'Funds an account for testing')
-  .addParam('address', 'The wallet address to fund', JALIL)
-  .setAction(async ({ address }, hre) => {
-    const [account] = await hre.viem.getWalletClients()
-
-    await account.sendTransaction({ to: address, value: parseEther('1') })
-  })
 
--- test/fixtures.ts
+++ test/fixtures.ts
@@ -1,18 +1,11 @@
 import { parseEther } from 'viem'
 import hre from 'hardhat'
 import SignatureRepositoryModule from '../ignition/modules/SignatureRepository'
-import { JALIL, VV } from './../helpers/constants'
 
 export async function signatureRepositoryFixture() {
   const [signer0, signer1, signer2, signer3] = await hre.viem.getWalletClients()
 
   const publicClient = await hre.viem.getPublicClient()
-
-  const testClient = await hre.viem.getTestClient()
-  await testClient.impersonateAccount({ address: JALIL })
-  await testClient.impersonateAccount({ address: VV })
-  await signer0.sendTransaction({ to: JALIL, value: parseEther('1') })
-  await signer0.sendTransaction({ to: VV, value: parseEther('1') })
 
   const { repository } = await hre.ignition.deploy(SignatureRepositoryModule)
 
